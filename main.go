d40a52acb75789dd6d2bd75d7bce2fdda6cdd13b
omari <zawdimario@gmail.com>
2023-11-28 20:20:43 +0900

    update paths

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/zawadimario/mysql-crud-api/pkg/database"
 	"github.com/zawadimario/mysql-crud-api/pkg/handlers"
+	"log"
+	"net/http"
 )
 
 func main() {
@@ -22,7 +21,7 @@ func main() {
 	router.HandleFunc("/albums/{id}", handlers.GetAlbumById).Methods(http.MethodGet)
 	router.HandleFunc("/albums/artist/{artist}", handlers.GetAlbumByArtist).Methods(http.MethodGet)
 	router.HandleFunc("/albums", handlers.CreateAlbum).Methods(http.MethodPost)
-	router.HandleFunc("/albums/{id}", handlers.UpdateAlbum).Methods(http.MethodPost)
+	router.HandleFunc("/albums/{id}", handlers.UpdateAlbum).Methods(http.MethodPut)
 	router.HandleFunc("/albums/{id}", handlers.DeleteAlbum).Methods(http.MethodDelete)
 
 	fmt.Printf("Starting server at port 8000\n")
